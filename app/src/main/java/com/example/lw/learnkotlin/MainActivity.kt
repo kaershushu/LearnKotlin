6e7b446790f1a7cdd7298c5889411f48f4d2da5b
alan <alan.leng@planetart.cn>
2018-11-22 18:15:38 +0800

    as:用于转型，类似cast

--- app/src/main/java/com/example/lw/learnkotlin/MainActivity.kt
+++ app/src/main/java/com/example/lw/learnkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lw.learnkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        text.text = "test"
+
+        // as 用于转型
+        val recycler = findViewById(R.id.recycler) as RecyclerView
+
+
     }
 }
