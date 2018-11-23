da4d0d3e0fe235cd688edb737f80de84d3104123
alan <alan.leng@planetart.cn>
2018-11-23 18:10:45 +0800

    API

--- app/src/main/java/com/example/lw/learnkotlin/request/RequestImpl.kt
+++ app/src/main/java/com/example/lw/learnkotlin/request/RequestImpl.kt
@@ -7,9 +7,10 @@ package com.example.lw.learnkotlin.request
 class RequestImpl(val cityId: String) : Request {
 
     companion object {
-        private val APP_ID = ""
-        private val URL = ""
-        private val CITY_ID = "$cityId"
+        private val APP_ID = "42f7eabaa769eff49da43ad20e9b0ff4"
+        private val URL = "https://samples.openweathermap.org/data/2.5/forecast?id="
+        private val CITY_ID = ""
+        private val COMPLET_URL = "$URL$CITY_ID&appid=$APP_ID"
     }
 
     override fun request(strategy: RequestStrategy): String = strategy.request()
