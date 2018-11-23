b3829d9d43cf8e52bd3dd5e4f6a3c10b8f29be58
alan <alan.leng@planetart.cn>
2018-11-23 10:03:03 +0800

    View和数据绑定

--- app/src/main/java/com/example/lw/learnkotlin/ForecastListAdapter.kt
+++ app/src/main/java/com/example/lw/learnkotlin/ForecastListAdapter.kt
@@ -10,17 +10,18 @@ import android.widget.TextView
  * Created on 2018/11/22.
  * @author Alan
  */
-class ForecastListAdapter(val arrays: List<String>): RecyclerView.Adapter<ForecastListAdapter.VH>() {
-    override fun onBindViewHolder(p0: VH, p1: Int) = Unit
+class ForecastListAdapter(private val arrays: List<String>) : RecyclerView.Adapter<ForecastListAdapter.VH>() {
+    override fun onBindViewHolder(p0: VH, p1: Int) {
+        p0.tv.text = arrays[p1]
+    }
 
     override fun getItemCount(): Int = arrays.size
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): VH = VH(LayoutInflater.from(p0.context).inflate(R.layout.layout_adapter, p0, false))
 
-    inner class VH(itemView: View): RecyclerView.ViewHolder(itemView) {
+    inner class VH(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        init {
-            val tv = itemView.findViewById(R.id.tv) as TextView
-        }
+        var tv: TextView = itemView.findViewById(R.id.tv) as TextView
+
     }
 }
